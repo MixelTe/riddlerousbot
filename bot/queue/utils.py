434c8bd418e29ec12e797d965780a125890a7a02
Mike <63655075+MixelTe@users.noreply.github.com>
2025-03-25 15:12:40 +0300

    fix

--- bot/queue/utils.py
+++ bot/queue/utils.py
@@ -7,42 +7,43 @@ from data.queue_user import QueueUser
 from utils import parse_int
 
 
-def updateQueue(bot: Bot, queue: Queue):
+def updateQueue(bot: Bot, queue: Queue, update_cur = True):
     txt = f"📝 Очередь {queue.name}:\n"
     txt += "-" * (len(txt) - 2) + "\n"
-    qus = QueueUser.all_in_queue(bot.db_sess, queue.id)
-    if len(qus) == 0:
-        txt += "Никого в очереди"
-        if queue.msg_next is not None:
-            tgapi.deleteMessage(queue.msg_next.chat_id, queue.msg_next.message_id)
-            queue.msg_next = None
-            bot.db_sess.commit()
-    else:
-        for i, qu in enumerate(qus):
-            user = qu.user
-            txt += f"{i+1}) @{user.username}\n"
-        if len(qus) > 1:
-            txt_next = f"🎞 Следующие в очереди {queue.name}\n🥇-> @{qus[0].user.username}\n🥈-> @{qus[1].user.username}"
-            if len(qus) == 3:
-                txt_next += f"\n💤 И ещё 1 ждущий"
-            elif len(qus) > 3:
-                txt_next += f"\n💤 И ещё {len(qus) - 2} ждущих"
+    if update_cur:
+        qus = QueueUser.all_in_queue(bot.db_sess, queue.id)
+        if len(qus) == 0:
+            txt += "Никого в очереди"
+            if queue.msg_next is not None:
+                tgapi.deleteMessage(queue.msg_next.chat_id, queue.msg_next.message_id)
+                queue.msg_next = None
+                bot.db_sess.commit()
         else:
-            txt_next = f"🎞 Следующий в очереди {queue.name}\n🥇-> @{qus[0].user.username}"
-        if queue.msg_next is not None:
-            tgapi.deleteMessage(queue.msg_next.chat_id, queue.msg_next.message_id)
-            queue.msg_next = None
-        btns = []
-        if len(qus) > 1:
-            btns.append(tgapi.InlineKeyboardButton.callback("Пропустить 1", f"queue_pass {queue.id}"))
-        btns.append(tgapi.InlineKeyboardButton.callback("Выйти", f"queue_exit {queue.id}"))
-        if len(qus) > 2:
-            btns.append(tgapi.InlineKeyboardButton.callback("Уйти в конец", f"queue_end {queue.id}"))
-        ok, r = bot.sendMessage(txt_next, reply_markup=tgapi.InlineKeyboardMarkup([btns]), message_thread_id=queue.msg.message_thread_id)
-        if not ok:
-            return "Error!"
-        queue.msg_next = Msg.new_from_data(bot.user, r)
-        bot.db_sess.commit()
+            for i, qu in enumerate(qus):
+                user = qu.user
+                txt += f"{i+1}) @{user.username}\n"
+            if len(qus) > 1:
+                txt_next = f"🎞 Следующие в очереди {queue.name}\n🥇-> @{qus[0].user.username}\n🥈-> @{qus[1].user.username}"
+                if len(qus) == 3:
+                    txt_next += f"\n💤 И ещё 1 ждущий"
+                elif len(qus) > 3:
+                    txt_next += f"\n💤 И ещё {len(qus) - 2} ждущих"
+            else:
+                txt_next = f"🎞 Следующий в очереди {queue.name}\n🥇-> @{qus[0].user.username}"
+            if queue.msg_next is not None:
+                tgapi.deleteMessage(queue.msg_next.chat_id, queue.msg_next.message_id)
+                queue.msg_next = None
+            btns = []
+            if len(qus) > 1:
+                btns.append(tgapi.InlineKeyboardButton.callback("Пропустить 1", f"queue_pass {queue.id}"))
+            btns.append(tgapi.InlineKeyboardButton.callback("Выйти", f"queue_exit {queue.id}"))
+            if len(qus) > 2:
+                btns.append(tgapi.InlineKeyboardButton.callback("Уйти в конец", f"queue_end {queue.id}"))
+            ok, r = bot.sendMessage(txt_next, reply_markup=tgapi.InlineKeyboardMarkup([btns]), message_thread_id=queue.msg.message_thread_id)
+            if not ok:
+                return "Error!"
+            queue.msg_next = Msg.new_from_data(bot.user, r)
+            bot.db_sess.commit()
 
     tgapi.editMessageText(queue.msg.chat_id, queue.msg.message_id, txt, reply_markup=tgapi.InlineKeyboardMarkup([[
         tgapi.InlineKeyboardButton.callback("Встать", f"queue_enter {queue.id}"),
@@ -104,5 +105,4 @@ class update_queue_msg_if_changes():
         else:
             if psecond_None != nsecond_None:
                 changes = True
-        if changes:
-            updateQueue(self.bot, self.queue)
+        updateQueue(self.bot, self.queue, changes)
